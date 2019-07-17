358e087dfa7a508833a62155bc1676523dad2bb2
hiltontj <trevor.hilton@gmail.com>
2019-07-16 20:28:13 -0400

    do the state changing nav bar

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,22 @@
 import React from 'react'
 
 export default class Popular extends React.Component {
+   constructor(props) {
+      super(props);
+
+      this.state = {
+         selectedLanguage : 'All'
+      };
+
+      this.updateLanguage = this.updateLanguage.bind(this);
+   }
+   
+   updateLanguage(selectedLanguage) {
+      this.setState({
+         selectedLanguage : selectedLanguage
+      })
+   }
+
    render() {
       const languages = [
          "All", "JavaScript", "Ruby", "Java", "CSS", "Python"
@@ -12,7 +28,10 @@ export default class Popular extends React.Component {
                languages.map((language) => {
                   return(
                      <li key={language}>
-                        <button className="btn-clear nav-link">
+                        <button 
+                        className="btn-clear nav-link"
+                        style={ language === this.state.selectedLanguage ? { color: 'rgb(35, 156, 196)' } : null }
+                        onClick={ () => this.updateLanguage(language) }>
                            {language}
                         </button>
                      </li>
