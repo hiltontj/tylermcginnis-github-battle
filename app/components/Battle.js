2280798fe943b592df8474bd796dd7ec62c31e95
hiltontj <trevor.hilton@gmail.com>
2019-08-24 23:25:31 -0400

    enable query string routing

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { FaUserFriends, FaFighterJet, FaTrophy, FaUser, FaTimesCircle } from 'react-icons/fa'
 import PropTypes from 'prop-types'
-import Results from './Results'
 import { ThemeConsumer } from '../contexts/theme'
+import { Link } from 'react-router-dom'
 
 function Instructions() {
    return (
@@ -133,8 +133,7 @@ export default class Battle extends React.Component {
 
       this.state = {
          playerOne: null,
-         playerTwo: null,
-         battle:    false
+         playerTwo: null
       }
 
       this.handleSubmit = this.handleSubmit.bind(this);
@@ -154,21 +153,7 @@ export default class Battle extends React.Component {
    }
 
    render() {
-      const { playerOne, playerTwo, battle } = this.state
-
-      if (battle) {
-         return (
-            <Results 
-               playerOne={playerOne} 
-               playerTwo={playerTwo} 
-               onReset={() => this.setState({
-                  playerOne : null,
-                  playerTwo: null,
-                  battle : false
-               })}
-            />
-         )
-      }
+      const { playerOne, playerTwo } = this.state
 
       return(
          <React.Fragment>
@@ -209,12 +194,15 @@ export default class Battle extends React.Component {
                   }
                </div>
                {playerOne && playerTwo && (
-                  <button 
-                    className="btn btn-dark btn-space"
-                    onClick={() => {this.setState({battle: true})}}
+                  <Link 
+                     className="btn btn-dark btn-space"
+                     to={{
+                        pathname: '/battle/results',
+                        search: `?playerOne=${playerOne}&playerTwo=${playerTwo}`
+                     }}
                   >
                      Battle!
-                  </button>
+                  </Link>
                )}
             </div>
          </React.Fragment>
--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import Popular from './components/Popular.js'
 import Battle from './components/Battle.js'
+import Results from './components/Results'
 import { ThemeProvider } from './contexts/theme.js'
 import Nav from './components/Nav.js'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
@@ -31,7 +32,8 @@ class App extends React.Component {
                      <Nav />
                      
                      <Route exact path='/' component={Popular} />
-                     <Route path='/battle' component={Battle} />
+                     <Route exact path='/battle' component={Battle} />
+                     <Route path='/battle/results' component={Results} />
                   </div>
                </div>
             </ThemeProvider>
